236f353d3b651f81bdf2414f306f3ab43c75ce9a
Stephanie <esceeaitchyu@gmail.com>
2018-10-04 10:22:02 -0700

    Complete User Story 6: Seeding Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# Create Users
+ 3.times do
+   user = User.create!(
+      email:  Faker::Internet.safe_email,
+      password: Faker::Internet.password(8)
+   )
+ end
+
+ users = User.all
+
+# Create Items
+  15.times do
+    item = Item.create!(
+      name: Faker::String.random,
+      user: users.sample
+    )
+  end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
