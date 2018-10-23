90bc4736718a912c021ec3233ca842dc2157b4a3
Stephanie <esceeaitchyu@gmail.com>
2018-10-23 13:48:22 -0700

    Move flash message into partial to work with AJAX

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,18 +6,18 @@ module ApplicationHelper
      content_tag :div, capture(&block), class: css_class
    end
 
-   def flash_message(flash)
-     if flash[:notice]
-        content_tag (:div, class: ["alert", "alert-success"]) do
-          button_tag(type: "button", class: "close", data: {dismiss: 'alert'}, "&times;")
-        end  
-        flash[:notice]
-     elsif flash[:alert]
-        content_tag (:div, class: ["alert", "alert-warning"]) do
-          button_tag(type: "button", class: "close", data: {dismiss: "alert"}, "&times;")
-        end
-        flash[:alert]
-     end
-   end
+#  def flash_message(flash)
+#     if flash[:notice]
+#        content_tag (:div, class: ["alert", "alert-success"]) do
+#          button_tag(type: "button", class: "close", :data => {dismiss: 'alert'}, "&times;")
+#        end  
+#        flash[:notice]
+#     elsif flash[:alert]
+#        content_tag (:div, class: ["alert", "alert-warning"]) do
+#          button_tag(type: "button", class: "close", :data => {dismiss: "alert"}, "&times;")
+#        end
+#        flash[:alert]
+#     end
+#   end
 
 end
